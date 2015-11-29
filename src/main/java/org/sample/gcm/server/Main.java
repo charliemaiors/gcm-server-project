0e17cd23b00b8937d470775bdeb06bddf28777c9
maa <pasquale.maiorano.picone@fokus.fraunhofer.de>
2015-11-29 14:07:06 +0100

    start!

---
+++ src/main/java/org/sample/gcm/server/Main.java
@@ -0,0 +1,18 @@
+package org.sample.gcm.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Carlo on 28/11/2015.
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main (String[] args){
+
+        SpringApplication.run(Main.class);
+
+    }
+
+}
