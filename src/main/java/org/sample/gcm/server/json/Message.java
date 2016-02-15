815a4b4d6150bdaa2dd2d3a032a1b3c8564914a0
maa <pasquale.maiorano.picone@fokus.fraunhofer.de>
2016-02-15 23:25:08 +0100

    fixed wrong extends

--- src/main/java/org/sample/gcm/server/json/Message.java
+++ src/main/java/org/sample/gcm/server/json/Message.java
@@ -8,7 +8,7 @@ public class Message extends AbstractMessage{
     private CustomData data;
 
     public Message(String to, String collapse_key, Long time_to_live, boolean delay_when_idle, CustomData data) {
-        super(to    , collapse_key, time_to_live, delay_when_idle);
+        super(to, collapse_key, time_to_live, delay_when_idle);
         this.data = data;
     }
 
--- src/main/java/org/sample/gcm/server/json/Notification.java
+++ src/main/java/org/sample/gcm/server/json/Notification.java
@@ -3,12 +3,12 @@ package org.sample.gcm.server.json;
 /**
  * Created by Carlo on 21/01/2016.
  */
-public class Notification extends Message{
+public class Notification extends AbstractMessage{
 
     private NotificationBody notification;
 
-    public Notification(String to, String message_id, String collapse_key, Long time_to_live, boolean delay_when_idle, CustomData data, NotificationBody notification) {
-        super(to, message_id, collapse_key, time_to_live, delay_when_idle, data);
+    public Notification(String to, String collapse_key, Long time_to_live, boolean delay_when_idle, NotificationBody notification) {
+        super(to,collapse_key,time_to_live,delay_when_idle);
         this.notification = notification;
     }
 
